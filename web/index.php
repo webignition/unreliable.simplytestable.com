5c620b74a2ac2150b94946fcc9c264119a23dff7
Jon Cram <jon@webignition.net>
2013-04-04 17:24:02 +0100

    Add rudimentary output, fix how it actually works

--- web/index.php
+++ web/index.php
@@ -5,19 +5,37 @@ $allowedErrors = array(
     500, 502, 503, 504
 );
 
-$error = isset($_GET['error']) && in_array($_GET['error'], $allowedErrors) ? (int)$_GET['error'] : false;
-if ($error === false) {
-    exit();
+$chosenStatusCode = isset($_GET['error']) && in_array($_GET['error'], $allowedErrors) ? (int)$_GET['error'] : false;
+if ($chosenStatusCode === false) {
+    $chosenStatusCode = 200;
 }
 
 $probability = filter_var($_GET['probability'], FILTER_VALIDATE_FLOAT);
 
 if ($probability < 0 || $probability > 1 || $probability === false) {
-    exit();
+    $probability = 1;
 }
 
-if ((rand(0, 100)/100) >= $probability) {    
-    header('HTTP/1.0 ' . $error);
-} else {
-    header('HTTP/1.0 200 OK');
-}
+$statusCode = ((rand(0, 100)/100) < $probability) ? $chosenStatusCode : 200;
+header('HTTP/1.0 ' . $statusCode);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Chosen status code: <?php echo $chosenStatusCode; ?>. Probability: <?php echo $probability; ?>. Outcome: <?php echo $statusCode; ?></title>
+  </head>
+  <body>
+      <p>
+          Chosen status code: <?php echo $chosenStatusCode; ?>
+      </p>
+      <p>
+          Probability: <?php echo $probability; ?>
+      </p>
+      <p>
+          Outcome: <?php echo $statusCode; ?>
+      </p>
+  </body>
+</html>
